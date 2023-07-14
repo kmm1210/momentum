4806a12b6906f0804e166758ac9daedb1693b978
kmm1210 <kmimi1210@gmail.com>
2023-07-14 18:57:44 +0900

    CSS 및 class활용

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
-const colors = ["#1abc9c", "#3498db", "#9b59b6", "#f39c12", "#e74c3c"];
+const body = document.querySelector("body");
 
-const title = document.querySelector("h2");
+function handleResize() {
+  const innerWidth = window.innerWidth;
+  const classNames = ["changeSkyblue", "changePlum", "changeYellow"];
 
-const superEventHandler = {
-  mouseenter: () => {
-    console.log("The mouse is here!");
-    title.style.color = colors[0];
-    title.innerText = "The mouse is here!";
-  },
-
-  mouseleave: () => {
-    console.log("The mouse is gone!");
-    title.style.color = colors[1];
-    title.innerText = "The mouse is gone!";
-  },
-
-  contextmenu: () => {
-    console.log("That was a right click!");
-    title.style.color = colors[2];
-    title.innerText = "That was a right click!";
-  },
-
-  resize: () => {
-    console.log("You just resize!");
-    title.style.color = colors[3];
-    title.innerText = "You just resize!";
+  if (innerWidth < 400) {
+    body.className = classNames[0];
+  } else if (innerWidth < 800) {
+    body.className = classNames[1];
+  } else {
+    body.className = classNames[2];
   }
-};
+}
 
-title.addEventListener("mouseenter", superEventHandler.mouseenter);
-title.addEventListener("mouseleave", superEventHandler.mouseleave);
-window.addEventListener("contextmenu", superEventHandler.contextmenu);
-window.addEventListener("resize", superEventHandler.resize);
+window.addEventListener("resize", handleResize);
